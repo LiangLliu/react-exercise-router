070c2d2a8ac00f2b94ced1f9cb05f03aafea0272
Liangliang Liu <max1141847753@gmail.com>
2020-08-12 23:14:41 +0800

    [Liangliang Liu] 添加 Home 页面

---
+++ src/exercise-1/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        <h1>This is a beautiful Home Page.</h1>
+        <p>And the url is '/'.</p>
+      </div>
+    )
+  }
+}
+
+export default Home;
