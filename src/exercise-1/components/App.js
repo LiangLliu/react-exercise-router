c5b907596c7c758e233731d313b5dfefd0a71cb0
Liangliang Liu <max1141847753@gmail.com>
2020-08-13 00:01:19 +0800

    [Liangliang Liu] add css and router

--- src/exercise-1/components/App.js
+++ src/exercise-1/components/App.js
@@ -1,12 +1,30 @@
 import React, {Component} from 'react';
 import '../styles/App.css';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router, Link, NavLink, Route, Switch} from 'react-router-dom';
+
+import Home from './Home';
+import MyProfile from './MyProfile';
+import AboutUs from './AboutUs';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <Router />
+        <Router>
+          <div className="header">
+            <NavLink className="header-link" activeClassName="select" to="/">Home</NavLink>
+            <NavLink className="header-link" activeClassName="select" to="/my-profile">My Profile</NavLink>
+            <NavLink className="header-link" activeClassName="select" to="/about-us">About Us</NavLink>
+          </div>
+
+          <Switch>
+            <Route exact path='/my-profile' component={MyProfile}/>
+            <Route exact path='/about-us' component={AboutUs}/>
+            <Route exact path='/' component={Home}/>
+            <Route path='*' component={Home}/>
+          </Switch>
+
+        </Router>
       </div>
     );
   }
