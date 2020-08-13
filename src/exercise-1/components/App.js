b70e4edf0ff2193170994d29a0839bf786bee84e
liangliang liu <max1141847753@gmail.com>
2020-08-13 14:58:44 +0800

    [Liangliang Liu] 代码重构

--- src/exercise-1/components/App.js
+++ src/exercise-1/components/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import '../styles/App.css';
-import {BrowserRouter as Router, Link, NavLink, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, NavLink, Route, Switch} from 'react-router-dom';
 
 import Home from './Home';
 import MyProfile from './MyProfile';
@@ -8,14 +8,35 @@ import AboutUs from './AboutUs';
 import Products from "./Products";
 import Product from "./Product";
 
+const urlPaths = [
+  "/my-profile",
+  "/about-us",
+  "/products",
+  "/goods",
+]
+
+const isUrlPaths = (pathname) => {
+  return !!urlPaths.find(it => it === pathname);
+}
+
 class App extends Component {
   render() {
     return (
       <div className="app">
         <Router>
           <div className="header">
-            <NavLink exact className="header-link" activeClassName="select" to="/">Home</NavLink>
-            <NavLink className="header-link" activeClassName="select" to="/products">Products</NavLink>
+
+            <NavLink exact to="/" className="header-link" activeClassName="select" isActive={
+              (match, location) => (match ||
+                (!isUrlPaths(location.pathname) &&
+                  !location.pathname.startsWith("/products/")))
+            }>Home
+            </NavLink>
+
+            <NavLink to="/products" className="header-link" activeClassName="select" isActive={
+              (match, location) => (match || location.pathname === "/goods")
+            }>Products
+            </NavLink>
             <NavLink className="header-link" activeClassName="select" to="/my-profile">My Profile</NavLink>
             <NavLink className="header-link" activeClassName="select" to="/about-us">About Us</NavLink>
           </div>
@@ -24,7 +45,7 @@ class App extends Component {
             <Route exact path='/' component={Home}/>
             <Route exact path='/my-profile' component={MyProfile}/>
             <Route exact path='/about-us' component={AboutUs}/>
-            <Route exact path={["/products", "/goods"]} component={Products} />
+            <Route exact path={["/products", "/goods"]} component={Products}/>
             <Route exact path='/products/:id' component={Product}/>
             <Route path='*' component={Home}/>
           </Switch>
